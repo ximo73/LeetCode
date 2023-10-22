19bd2512e70fe6fa381f6bf34f59c9690fe8165c
ximo73 <ximobecerra@gmail.com>
2023-10-22 02:43:51 -0300

    mediante

---
+++ 2390. Removing Stars From a String/RemovingStarsFromaString.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class RemovingStarsFromaString {
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        Stack<Character> aux = new Stack<Character>();
+        String res = "";
+        for (int i = s.length()-1; i >= 0; i--) {
+            stack.push(s.charAt(i));
+        }
+        
+        //se bugea cuando hay 2 ** seguidos
+        while (!stack.isEmpty() ) {
+            if (stack.peek() == '*') {
+                aux.pop();
+            }else{
+                aux.push(stack.peek());
+            }	
+            stack.pop();
+        }
+        /* 
+        for(int i = stack.size() - 1; i >= 0; i--){
+            res += stack.get(i);
+        }
+        */
+        for(int i = 0; i < aux.size(); i++){
+            res += aux.get(i);
+        }
+        return res;
+    }
+
+    public static void main(String args[]){  
+        RemovingStarsFromaString r = new RemovingStarsFromaString();
+        System.out.println("resultado: "+r.removeStars("leet**cod*e"));
+    }  
+}
